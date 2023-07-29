c3a4316e7f0ce67a533f47a855b63c4daaa849a6
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2023-07-30 00:06:33 +0600

    complete the footer section

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,56 +1,46 @@
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+
 const Footer = () => {
   const today = new Date();
   const year = today.getFullYear();
   return (
     <footer>
-      <div className="footer py-[97px] px-[305px] text-neutral-content">
-        <div className="bg-[#1F2937] w-full">
-          <p>
-            ACME Industries Ltd.
-            <br />
-            Providing reliable tech since 1992
-          </p>
+      <div className="flex justify-center text-white text-center">
+        <div className="bg-[#1F2937] py-[97px] w-full">
+          <h3 className="text-[32px] font-medium mb-[10px]">CONTACT US</h3>
+          <div className="text-[20px] font-medium">
+            <p>567 Wajed Street, Uni 27, Bangladesh</p>
+            <p>+88 01784009104</p>
+            <p>Mon - Fri: 08:00 - 22:00</p>
+            <p>Sat - Sun: 10:00 - 23:00</p>
+          </div>
         </div>
-        <div className="bg-[#111827] w-full">
-          <span className="footer-title">Social</span>
-          <div className="grid grid-flow-col gap-4">
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
-            </a>
+        <div className="bg-[#111827] py-[97px] w-full">
+          <h3 className="text-[32px] font-medium mb-[10px]">Follow US</h3>
+          <p className="text-[20px] font-medium mb-[22px]">
+            Join us on social media
+          </p>
+          <div className="flex justify-center gap-[32px] items-center">
+            <Link target="_blank" to="https://www.facebook.com">
+              <span className="text-[33px] ">
+                <FaFacebookF />
+              </span>
+            </Link>
+            <Link target="_blank" to="https://www.instagram.com">
+              <span className="text-[35px]">
+                <FaInstagram />
+              </span>
+            </Link>
+            <Link target="_blank" to="https://www.twitter.com">
+              <span className="text-[35px]">
+                <FaTwitter />
+              </span>
+            </Link>
           </div>
         </div>
       </div>
-      <div className="text-[20px] font-medium  text-center text-white py-[17px] bg-[#151515] ">
+      <div className="bg-[#151515] text-[20px] font-medium  text-center text-white py-[17px] ">
         <div>
           <p>Copyright © {year} Breezy Bites - All right reserved.</p>
         </div>
